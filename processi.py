ee45ac0c9892ed025d2b5e81ced86d6a54a7d9c4
Martin Anske <martin.anske@convema.com>
2023-01-19 17:16:01 +0100

    benchmark and window in seperate classes

--- processi.py
+++ processi.py
@@ -4,101 +4,107 @@ import random
 import math
 
 
-# flag only for state of button, since processes don't share memory
-running = False
-# list for terminating processes
-all_processes = []
+class ProcessBenchmark:
+	def __init__(self, window):
+		self.window = window
+		self.running = False  # flag only for state of button, since processes don't share memory
+		self.all_processes = []  # list for terminating processes
+
+	# backup function for simulating stress
+	@staticmethod
+	def bench(number):
+		print("Process ", number, " started.")
+		i = 0
+		while True:
+			i += 1
+
+	# approximating pi
+	@staticmethod
+	def pi(number):
+		print("Process ", number, " started.")
+		innerhalb = 0
+		anzahl = 0
+		while True:
+			anzahl += 1
+			x = random.random()
+			y = random.random()
+			z = math.sqrt(math.pow(x, 2)+math.pow(y, 2))
+			if z < 1:
+				innerhalb += 1
+			pi_calculated = 4*innerhalb/anzahl
+			if anzahl % 10000000 == 0:
+				print("Process ", number, ", Pi: ", pi_calculated)
+
+	def start(self):
+		if self.window.entryProcesses.get() == "":
+			processes = 0
+		else:
+			processes = int(self.window.entryProcesses.get())
+
+		if self.running:
+			for process in self.all_processes:
+				process.terminate()
+			self.all_processes.clear()
+			self.running = False
+			print("All processes killed.")
+			self.window.labelStatus.config(text="Idle")
+			self.window.buttonStart.config(text="Start")
+			self.window.labelCount.config(text="")
+			self.window.entryProcesses.config(state="normal")
+		else:
+			if processes > 0:
+				self.running = True
+				for i in range(processes):
+					process = multiprocessing.Process(target=self.pi, args=(i+1,))
+					process.start()
+					self.all_processes.append(process)
+				self.window.labelStatus.config(text="Running")
+				self.window.buttonStart.config(text="Stop")
+				self.window.labelCount.config(text=str(processes)+" process(es)")
+				self.window.entryProcesses.config(state="disabled")
 
 
-# backup function for simulating stress
-def bench(number):
-	print("Process ", number, " started.")
-	i = 0
-	while True:
-		i += 1
+class Window(tk.Frame):
+	def __init__(self, parent):
+		tk.Frame.__init__(self, parent)
+		self.validation = self.register(self.only_numbers)
+
+		self.labelProcesses = tk.Label(self, text="Number of processes", font=("Arial", 15))
+		self.labelProcesses.pack(pady=2)
+		self.entryProcesses = tk.Entry(self, font=("Arial", 15), validate="key", validatecommand=(self.validation, "%S"))
+		self.entryProcesses.focus()
+		self.entryProcesses.pack(pady=2)
+		self.buttonStart = tk.Button(self, text="Start", font=("Arial", 15))
+		self.buttonStart.pack(pady=2)
+		self.labelStatus = tk.Label(self, text="Idle", font=("Arial", 20))
+		self.labelStatus.pack(pady=2)
+		self.labelCount = tk.Label(self, text="", font=("Arial", 20))
+		self.labelCount.pack(pady=2)
+		self.pack()
+
+	@staticmethod
+	def only_numbers(char):
+		return char.isdigit() or char == "\b"
 
 
-# approximating pi
-def pi(number):
-	print("Process ", number, " started.")
-	innerhalb = 0
-	anzahl = 0
-	while True:
-		anzahl += 1
-		x = random.random()
-		y = random.random()
-		z = math.sqrt(math.pow(x, 2)+math.pow(y, 2))
-		if z < 1:
-			innerhalb += 1
-		pi_calculated = 4*innerhalb/anzahl
-		if anzahl % 10000000 == 0:
-			print("Process ", number, ", Pi: ", pi_calculated)
+if __name__ == "__main__":
+	root = tk.Tk()
+	root.title("Processi")
+	root.geometry("300x200")
+	root.resizable(False, False)
+
+	window = Window(root)
+	benchmark = ProcessBenchmark(window)
+	window.buttonStart.configure(command=benchmark.start)
 
 
-def only_numbers(char):
-	return char.isdigit()
-
-
-def start():
-	global running
-	global all_processes
-
-	if entryProcesses.get() == "":
-		processes = 0
-	else:
-		processes = int(entryProcesses.get())
-
-	if running:
-		for process in all_processes:
+	def on_closing():
+		for process in benchmark.all_processes:
 			process.terminate()
-		all_processes.clear()
-		running = False
+		benchmark.all_processes.clear()
 		print("All processes killed.")
-		labelStatus.config(text="Idle")
-		buttonStart.config(text="Start")
-		labelCount.config(text="")
-		entryProcesses.config(state="normal")
-	else:
-		if processes > 0:
-			running = True
-			for i in range(processes):
-				process = multiprocessing.Process(target=pi, args=(i+1,))
-				process.start()
-				all_processes.append(process)
-			labelStatus.config(text="Running")
-			buttonStart.config(text="Stop")
-			labelCount.config(text=str(processes)+" process(es)")
-			entryProcesses.config(state="disabled")
+		root.destroy()
 
 
-window = tk.Tk()
-window.title("Processi")
-window.geometry("300x200")
-window.resizable(False, False)
-
-validation = window.register(only_numbers)
-
-labelProcesses = tk.Label(text="Number of processes", font=("Arial", 15))
-labelProcesses.pack(pady=2)
-entryProcesses = tk.Entry(window, font=("Arial", 15), validate="key", validatecommand=(validation, "%S"))
-entryProcesses.focus()
-entryProcesses.pack(pady=2)
-buttonStart = tk.Button(window, text="Start", font=("Arial", 15), command=start)
-buttonStart.pack(pady=2)
-labelStatus = tk.Label(text="Idle", font=("Arial", 20))
-labelStatus.pack(pady=2)
-labelCount = tk.Label(text="", font=("Arial", 20))
-labelCount.pack(pady=2)
-
-
-def on_closing():
-	global all_processes
-	for process in all_processes:
-		process.terminate()
-	all_processes.clear()
-	print("All processes killed.")
-	window.destroy()
-
-
-window.protocol("WM_DELETE_WINDOW", on_closing)
-window.mainloop()
+	root.protocol("WM_DELETE_WINDOW", on_closing)
+	root.mainloop()
--- threaddi.py
+++ threaddi.py
@@ -4,99 +4,104 @@ import random
 import math
 
 
-# flag for killing threads and state of button
-running = False
+class ThreadBenchmark:
+	def __init__(self, window):
+		self.window = window
+		self.running = False  # flag for killing threads and state of button
+
+	# backup function for simulating stress
+	def bench(self, number):
+		print("Process ", number, " started.")
+		i = 0
+		while True:
+			if not self.running:
+				break
+			i += 1
+
+	# approximating pi
+	def pi(self, number):
+		print("Thread ", number, " started.")
+		innerhalb = 0
+		anzahl = 0
+		while True:
+			anzahl += 1
+			x = random.random()
+			y = random.random()
+			z = math.sqrt(math.pow(x, 2)+math.pow(y, 2))
+			if z < 1:
+				innerhalb += 1
+			pi_calculated = 4*innerhalb/anzahl
+			if not self.running:
+				# print("Thread ", number, ", Pi: ", pi_calculated)
+				# printing this was too much at the same time for simultaneous threads
+				print(pi_calculated)
+				break
+
+	def start(self):
+		if self.window.entryThreads.get() == "":
+			threads = 0
+		else:
+			threads = int(self.window.entryThreads.get())
+
+		if self.running:
+			self.running = False
+			print("All threads killed.")
+			self.window.labelStatus.config(text="Idle")
+			self.window.buttonStart.config(text="Start")
+			self.window.labelCount.config(text="")
+			self.window.entryThreads.config(state="normal")
+		else:
+			if threads > 0:
+				self.running = True
+				for i in range(threads):
+					thread = Thread(target=self.pi, args=(i+1,))
+					thread.daemon = True
+					thread.start()
+				self.window.labelStatus.config(text="Running")
+				self.window.buttonStart.config(text="Stop")
+				self.window.labelCount.config(text=str(threads)+" Thread(s)")
+				self.window.entryThreads.config(state="disabled")
 
 
-# backup function for simulating stress
-def bench(number):
-	print("Process ", number, " started.")
-	i = 0
-	while True:
-		global running
-		if not running:
-			break
-		i += 1
+class Window(tk.Frame):
+	def __init__(self, parent):
+		tk.Frame.__init__(self, parent)
+		self.validation = self.register(self.only_numbers)
+
+		self.labelThreads = tk.Label(self, text="Number of threads", font=("Arial", 15))
+		self.labelThreads.pack(pady=2)
+		self.entryThreads = tk.Entry(self, font=("Arial", 15), validate="key", validatecommand=(self.validation, "%S"))
+		self.entryThreads.focus()
+		self.entryThreads.pack(pady=2)
+		self.buttonStart = tk.Button(self, text="Start", font=("Arial", 15))
+		self.buttonStart.pack(pady=2)
+		self.labelStatus = tk.Label(self, text="Idle", font=("Arial", 20))
+		self.labelStatus.pack(pady=2)
+		self.labelCount = tk.Label(self, text="", font=("Arial", 20))
+		self.labelCount.pack(pady=2)
+		self.pack()
+
+	@staticmethod
+	def only_numbers(char):
+		return char.isdigit() or char == "\b"
 
 
-# approximating pi
-def pi(number):
-	print("Thread ", number, " started.")
-	innerhalb = 0
-	anzahl = 0
-	while True:
-		anzahl += 1
-		x = random.random()
-		y = random.random()
-		z = math.sqrt(math.pow(x, 2)+math.pow(y, 2))
-		if z < 1:
-			innerhalb += 1
-		pi_calculated = 4*innerhalb/anzahl
-		global running
-		if not running:
-			# print("Thread ", number, ", Pi: ", pi_calculated)
-			# printing this was too much at the same time for simultaneous threads
-			print(pi_calculated)
-			break
+if __name__ == "__main__":
+	root = tk.Tk()
+	root.title("Threaddi")
+	root.geometry("300x200")
+	root.resizable(False, False)
+
+	window = Window(root)
+	benchmark = ThreadBenchmark(window)
+	window.buttonStart.configure(command=benchmark.start)
 
 
-def only_numbers(char):
-	return char.isdigit()
-
-
-def start():
-	global running
-
-	if entryThreads.get() == "":
-		threads = 0
-	else:
-		threads = int(entryThreads.get())
-
-	if running:
-		running = False
+	def on_closing():
+		# threads killed automatically since daemon-threads
 		print("All threads killed.")
-		labelStatus.config(text="Idle")
-		buttonStart.config(text="Start")
-		labelCount.config(text="")
-		entryThreads.config(state="normal")
-	else:
-		if threads > 0:
-			running = True
-			for i in range(threads):
-				thread = Thread(target=pi, args=(i+1,))
-				thread.daemon = True
-				thread.start()
-			labelStatus.config(text="Running")
-			buttonStart.config(text="Stop")
-			labelCount.config(text=str(threads)+" Thread(s)")
-			entryThreads.config(state="disabled")
+		root.destroy()
 
 
-window = tk.Tk()
-window.title("Threaddi")
-window.geometry("300x200")
-window.resizable(False, False)
-
-validation = window.register(only_numbers)
-
-labelThreads = tk.Label(text="Number of threads", font=("Arial", 15))
-labelThreads.pack(pady=2)
-entryThreads = tk.Entry(window, font=("Arial", 15), validate="key", validatecommand=(validation, "%S"))
-entryThreads.focus()
-entryThreads.pack(pady=2)
-buttonStart = tk.Button(window, text="Start", font=("Arial", 15), command=start)
-buttonStart.pack(pady=2)
-labelStatus = tk.Label(text="Idle", font=("Arial", 20))
-labelStatus.pack(pady=2)
-labelCount = tk.Label(text="", font=("Arial", 20))
-labelCount.pack(pady=2)
-
-
-def on_closing():
-	# threads killed automatically since daemon-threads
-	print("All threads killed.")
-	window.destroy()
-
-
-window.protocol("WM_DELETE_WINDOW", on_closing)
-window.mainloop()
+	root.protocol("WM_DELETE_WINDOW", on_closing)
+	root.mainloop()
